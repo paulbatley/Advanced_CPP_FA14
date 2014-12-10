5b66818f4d8bde9457c957a83512a0435dbda859
elma <elmapayong@gmail.com>
2014-12-10 14:04:49 -0800

    can render room

--- ACPP_Project/Room.cpp
+++ ACPP_Project/Room.cpp
@@ -10,7 +10,7 @@ Room::Room(int x, int y){
 	coord.x = x;
 	coord.y = y;
 
-	int wallLimit = (((2 * (height - 2)) + (2 * width)) / 4);
+	int wallLimit = (((height*width) - (2 * (height - 2)) + (2 * width)) / 4);
 
 	filename = "map.map";
 	stringstream subfilename;
@@ -18,31 +18,36 @@ Room::Room(int x, int y){
 	filename.insert(3, subfilename.str());
 	cout << filename << endl;
 
+
 	ofstream file(filename);
 	for (int i = 0; i < height; ++i)
 	{
 		int toLimit = 0;
 		for (int j = 0; j < width; ++j)
 		{
-			if ((i == (height / 2) && (j == 0 || j == (width - 1))) || (j == (width / 2) && (i == 0 || i == (height - 1))))
-			{
-				file << FillNumber(1) << " ";
-			}
-			else if (i == 0 || i == (height - 1) || j == 0 || j == (width - 1) && (i != (height / 2) || j != (width / 2)))
-			{
+			if (i == 0 && j == (width / 2))// up
 				file << FillNumber(0) << " ";
-			}
-			else
+			else if (i == (height / 2) && j == (0)) // left
+				file << FillNumber(1) << " ";
+			else if (i == (height - 1) && j == (width / 2)) // down
+				file << FillNumber(3) << " ";
+			else if (i == height / 2 && j == (width - 1)) //right
+				file << FillNumber(2) << " ";
+			else if (i == 0 || i == (height - 1) || j == 0 || j == (width - 1) && (i != (height / 2) || j != (width / 2)))//wall
 			{
-				int num = rand() % TOTAL_TILE_SPRITES;
-				if (num == 0 && toLimit != wallLimit)
+				file << FillNumber(4) << " ";
+			}
+			else//rest of floor
+			{
+				int num = rand() % (TOTAL_TILE_SPRITES - 4) + 4;
+				if (num == 4 && toLimit < wallLimit)
 				{
 					file << FillNumber(num) << " ";
 					++toLimit;
 				}
-				else if (num == 0 && toLimit == wallLimit)
+				else if (num == 4 && toLimit >= wallLimit)
 				{
-					num = (rand() % (TOTAL_TILE_SPRITES - 1)) + 1;
+					num = (rand() % (TOTAL_TILE_SPRITES - 5)) + 5;
 					file << FillNumber(num) << " ";
 				}
 				else
